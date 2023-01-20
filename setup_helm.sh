b5e4a59ebbd2bb63592e43db0a61014f8d67d815
supachai phobpimai <611463023@crru.ac.th>
2023-01-20 11:33:56 +0700

    better command

--- setup_helm.sh
+++ setup_helm.sh
@@ -13,6 +13,9 @@ helm repo update
 # ตั้งค่าให้ helm ใช้ namespace and 
 # เปลี่ยน namespace ได้ตามต้องการ ถ้าไม่มีก็ create ใหม่
 # kubectl create namespace and
-helm install ingress1 ingress-nginx/ingress-nginx --namespace and --set controller.ingressClassResource.name=class1
+helm upgrade --install ingress1 ingress-nginx  --repo https://kubernetes.github.io/ingress-nginx  --namespace and --create-namespace --set controller.ingressClassResource.name=class1  --set controller.admissionWebhooks.enabled=false --set controller.service.nodePorts.http=30080 --set controller.service.nodePorts.https=30443 
+
 # ถ้าชื่อ ingress1 ซ้ำกับอันเก่า ให้ลบอันเก่าก่อน
-# helm uninstall ingress1 --namespace and
+helm uninstall ingress1 --namespace and
+
+# kubectl --namespace and get services -o wide -w ingress1-ingress-nginx-controller
